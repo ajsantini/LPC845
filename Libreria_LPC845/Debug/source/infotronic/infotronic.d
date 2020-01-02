0a318dbf43ef9a2bf71e84804c9faa0bdd4534fa
Augusto <santini_augusto@hotmail.com>
2020-01-02 17:30:38 -0300

    Correccion de errores en maquina de timers, agregado de LCD

--- Libreria_LPC845/Debug/source/infotronic/infotronic.d
+++ Libreria_LPC845/Debug/source/infotronic/infotronic.d
@@ -1,29 +1,32 @@
 source/infotronic/infotronic.o source/infotronic/infotronic.d: \
  ../source/infotronic/infotronic.c \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_IOCON.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_GPIO.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_SYSTICK.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_ADC.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/teclado.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/relays.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/display.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/termometro.h \
- /home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/timer.h
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_IOCON.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_GPIO.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_SYSTICK.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_ADC.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/teclado.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/relays.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/display.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/termometro.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/timer.h \
+ /home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/LCD.h
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_IOCON.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_IOCON.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_GPIO.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_GPIO.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_SYSTICK.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_SYSTICK.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl/HPL_ADC.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl/HPL_ADC.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/teclado.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/teclado.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/relays.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/relays.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/display.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/display.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/termometro.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/termometro.h:
 
-/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic/timer.h:
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/timer.h:
+
+/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic/LCD.h:
--- Libreria_LPC845/Debug/source/infotronic/subdir.mk
+++ Libreria_LPC845/Debug/source/infotronic/subdir.mk
@@ -4,6 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../source/infotronic/LCD.c \
 ../source/infotronic/display.c \
 ../source/infotronic/infotronic.c \
 ../source/infotronic/relays.c \
@@ -12,6 +13,7 @@ C_SRCS += \
 ../source/infotronic/timer.c 
 
 OBJS += \
+./source/infotronic/LCD.o \
 ./source/infotronic/display.o \
 ./source/infotronic/infotronic.o \
 ./source/infotronic/relays.o \
@@ -20,6 +22,7 @@ OBJS += \
 ./source/infotronic/timer.o 
 
 C_DEPS += \
+./source/infotronic/LCD.d \
 ./source/infotronic/display.d \
 ./source/infotronic/infotronic.d \
 ./source/infotronic/relays.d \
@@ -32,7 +35,7 @@ C_DEPS += \
 source/infotronic/%.o: ../source/infotronic/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -D__REDLIB__ -DCPU_LPC845M301JBD48 -DCPU_LPC845M301JBD48_cm0plus -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -D__MCUXPRESSO -DDEBUG -I"/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes" -I"/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/infotronic" -I"/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hpl" -I"/home/augusto/Desktop/InfoII/Libreria_AJS/Libreria_LPC845/includes/hri" -O0 -fno-common -g3 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -D__REDLIB__ -DCPU_LPC845M301JBD48 -DCPU_LPC845M301JBD48_cm0plus -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -D__MCUXPRESSO -DDEBUG -I"/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes" -I"/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/infotronic" -I"/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hpl" -I"/home/augusto/Desktop/InfoII/Libreria_LPC845/LPC845/Libreria_LPC845/includes/hri" -O0 -fno-common -g3 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
