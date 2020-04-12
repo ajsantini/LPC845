8ecc1add70fa89d0f403bb8fea91912ca0ec90b3
Esteban Eduardo Chiama <eechiama@gmail.com>
2020-04-12 20:40:52 -0300

    Más documentación de periférico ACMP.

--- Libreria_LPC845/includes/hal/HAL_ACMP.h
+++ Libreria_LPC845/includes/hal/HAL_ACMP.h
@@ -36,7 +36,7 @@
  *
  * La tensión de salida de la escalera puede ser entonces, en función de la tensión de referencia:
  *
- * \f$ \frac{n * Vref}{31} \f$
+ * \f$ \huge  \frac{n * Vref}{31} \f$
  *
  * n: Entero entre 0 y 31.
  *
@@ -47,71 +47,72 @@
  *
  * Por ejemplo, es posible de esta manera disparar conversiones analógicas del periférico ADC.
  *
+ * @{
  */
 
 #ifndef HAL_ACMP_H_
 #define HAL_ACMP_H_
 
-/** Selección, o no, de sincronismo de señal de salida del comparador con clock de bus.*/
+/** Selección de sincronismo de la señal digital de salida del comparador analógico.*/
 typedef enum
 {
-	HAL_ACMP_OUTPUT_DIRECT = 0,
-	HAL_ACMP_OUTPUT_SYNC,
+	HAL_ACMP_OUTPUT_DIRECT = 0, /**< Los cambios se reflejarán instantáneamente.*/
+	HAL_ACMP_OUTPUT_SYNC /**< Los cambios serán sincronizados con el clock de bus.*/
 }hal_acmp_output_control_en;
 
 /** Posible umbral de histéresis para cambios a la salida del comparador analógico.*/
 typedef enum
 {
-	HAL_ACMP_HYSTERESIS_NONE = 0,
-	HAL_ACMP_HYSTERESIS_5mV,
-	HAL_ACMP_HYSTERESIS_10mV,
-	HAL_ACMP_HYSTERESIS_20mV
+	HAL_ACMP_HYSTERESIS_NONE = 0, /**< Sin histéresis.*/
+	HAL_ACMP_HYSTERESIS_5mV, /**< Umbral de 5mV.*/
+	HAL_ACMP_HYSTERESIS_10mV, /**< Umbral de 10mV. */
+	HAL_ACMP_HYSTERESIS_20mV /**< Umbral de 20mV. */
 }hal_acmp_hysteresis_sel_en;
 
 /** Posibles tensiones de referencia para la escalera de tensión del comparador analógico.*/
 typedef enum
 {
-	HAL_ACMP_LADDER_VREF_VDD = 0,
-	HAL_ACMP_LADDER_VREF_VDDCMP_PIN
+	HAL_ACMP_LADDER_VREF_VDD = 0, /**< Tensión de referencia VDD.*/
+	HAL_ACMP_LADDER_VREF_VDDCMP_PIN /**< Tensión de referencia tomada del pin externo con función analógica VDDCMP.*/
 }hal_acmp_ladder_vref_sel_en;
 
 /** Selección de tipo de flanco de la señal de salida
  * del comparador analógico que será trigger de interrupción. */
 typedef enum
 {
-	HAL_ACMP_EDGE_FALLING = 0,
-	HAL_ACMP_EDGE_RISING,
-	HAL_ACMP_EDGE_BOTH
+	HAL_ACMP_EDGE_FALLING = 0, /**< Flanco descendente.*/
+	HAL_ACMP_EDGE_RISING, /**< Flanco ascendente.*/
+	HAL_ACMP_EDGE_BOTH /**< Ambos tipos de flanco.*/
 }hal_acmp_edge_sel_en;
 
 /** Posibles entradas al comparador analógico, válidas tanto para la positiva como la negativa.*/
 typedef enum
 {
-	HAL_ACMP_INPUT_VOLTAGE_VLADDER_OUT = 0,
-	HAL_ACMP_INPUT_VOLTAGE_ACMP_I1,
-	HAL_ACMP_INPUT_VOLTAGE_ACMP_I2,
-	HAL_ACMP_INPUT_VOLTAGE_ACMP_I3,
-	HAL_ACMP_INPUT_VOLTAGE_ACMP_I4,
-	HAL_ACMP_INPUT_VOLTAGE_ACMP_I5,
-	HAL_ACMP_INPUT_VOLTAGE_BANDGAP,
-	HAL_ACMP_INPUT_VOLTAGE_DACOUT0
+	HAL_ACMP_INPUT_VOLTAGE_VLADDER_OUT = 0, /**< Tensión interna de salida de la escalera de tensión.*/
+	HAL_ACMP_INPUT_VOLTAGE_ACMP_I1, /**< Pin externo con función analógica ACMP_I1.*/
+	HAL_ACMP_INPUT_VOLTAGE_ACMP_I2, /**< Pin externo con función analógica ACMP_I2.*/
+	HAL_ACMP_INPUT_VOLTAGE_ACMP_I3, /**< Pin externo con función analógica ACMP_I3.*/
+	HAL_ACMP_INPUT_VOLTAGE_ACMP_I4, /**< Pin externo con función analógica ACMP_I4.*/
+	HAL_ACMP_INPUT_VOLTAGE_ACMP_I5, /**< Pin externo con función analógica ACMP_I5.*/
+	HAL_ACMP_INPUT_VOLTAGE_BANDGAP, /**< Tensión interna de referencia 'Bandgap'.*/
+	HAL_ACMP_INPUT_VOLTAGE_DACOUT0 /**< Señal interna, salida 0 del periférico DAC.*/
 }hal_acmp_input_voltage_sel_en;
 
 /** Estructura de configuración de características generales del comparador analógico.*/
 typedef struct
 {
-	hal_acmp_output_control_en output_control;
-	hal_acmp_hysteresis_sel_en hysteresis;
-	uint8_t interrupt_enable;
-	hal_acmp_edge_sel_en edge_sel;
+	hal_acmp_output_control_en output_control; /**< Control de sincronismo de la señal de salida del comparador.*/
+	hal_acmp_hysteresis_sel_en hysteresis; /**< Configura umbral de histérisis para reflejar cambios en la salida del comparador.*/
+	uint8_t interrupt_enable; /**< Habilita/deshabilita interrupciones generadas por la salida del comparador.*/
+	hal_acmp_edge_sel_en edge_sel; /**< Configura el tipo de flanco de la señal de salida del comparador que genera un pedido de interrupción.*/
 }hal_acpm_config_t;
 
 /** Estructura de configuración de la escalera de tensión del comparador analógico.*/
 typedef struct
 {
-	uint8_t enable;
-	hal_acmp_ladder_vref_sel_en vref_sel;
-	uint8_t step;
+	uint8_t enable; /**< Habilitación/deshabilitación de la escalera de tensión.*/
+	hal_acmp_ladder_vref_sel_en vref_sel; /**< Selección de tensión de referencia de la escalera de tensión.*/
+	uint8_t step; /**< Configura qué fracción de la tensión de referencia estará será la tensión de salida de la escalera de tensión.*/
 }hal_acmp_ladder_config_t;
 
 /**
@@ -178,3 +179,7 @@ void hal_acmp_ladder_config(const hal_acmp_ladder_config_t *config);
 void hal_acmp_input_select(hal_acmp_input_voltage_sel_en positive_input, hal_acmp_input_voltage_sel_en negative_input);
 
 #endif /* HAL_ACMP_H_ */
+
+/**
+ * @}
+ */
